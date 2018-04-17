79787ee11da42ff75ed49cf2da6a9eb18ba2b712
DESKTOP-FIPOO7U\cachorro <mario.martinez.zavala@gmail.com>
2018-04-17 17:27:10 -0500

    fix: se agregó la propiedad google en el listado de usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -13,7 +13,7 @@ app.get("/", mdAutenticacion.verificaToken, (req, res, next) => {
   var desde = req.query.desde || 0;
   desde = Number(desde);
 
-  Usuario.find({}, "nombre email img role")
+  Usuario.find({}, "nombre email img role google")
     .skip(desde)
     .limit(5)
     .exec((err, usuarios) => {
